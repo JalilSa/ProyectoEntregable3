a5270a619ac47b20771bfee6db53bce7dddc698d
JalilSa <95229047+JalilSa@users.noreply.github.com>
2023-05-23 21:20:52 -0300

    Primera entrega de websockets

--- app.js
+++ app.js
@@ -1,16 +1,49 @@
-const express = require('express');
+import express from 'express'
+import { Server as SocketIOServer } from 'socket.io';
+import { createServer } from 'http';
+import handlebars from 'express-handlebars'
+import productsRouter from './routes/products';
+import cartsRouter from './routes/carts';
+
+const PORT = 8080;
 const app = express();
+app.use(express.json());
 
-const productsRouter = require('./routes/products');
-const cartsRouter = require('./routes/carts');
-
-app.use(express.json()); // Middleware para parsear JSON en el body de las peticiones
+// Config de handlebars
+app.engine('handlebars', handlebars());
+app.set('view engine', 'handlebars');
+app.use(express.static('public'));
 
 app.use('/api/products', productsRouter);
 app.use('/api/carts', cartsRouter);
 
-const PORT = 8080;
+// Rutas de vistas
+app.get('/', (req, res) => {
+  res.render('home');
+});
 
-app.listen(PORT, () => {
+app.get('/realtimeproducts', (req, res) => {
+  res.render('realTimeProducts');
+});
+
+const httpServer = createServer(app);
+const io = new SocketIOServer(httpServer);
+
+// Config de socket.io
+io.on('connection', (socket) => {
+  console.log('Nuevo usuario conectado');
+  
+  socket.on('newProduct', (product) => {
+    pm.addProduct(product.title, product.description, product.price, product.thumbnail, product.code, product.stock);
+    io.emit('updateProducts', pm.getProducts());
+  });
+
+  socket.on('deleteProduct', (productId) => {
+    pm.deleteProduct(parseInt(productId));
+    io.emit('updateProducts', pm.getProducts());
+  });
+});
+
+httpServer.listen(PORT, () => {
   console.log(`Servidor express corriendo en el puerto ${PORT}`);
 });
